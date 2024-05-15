522b1b95f58ee007107c0abea859422ae61789f2
Deepanshu Verma <init.deepanshu@gmail.com>
2024-05-15 13:39:36 +0530

    complete process

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    genres = models.CharField(max_length=500, null=True,blank=True)
+    biography = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
